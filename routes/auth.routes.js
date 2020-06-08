19087469cf516d212f7e7ed8596e564a13684d65
Vladislav <vladovinkin@gmai.com>
2020-06-09 01:14:35 +0300

    корректировка файлов настройки

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express'); // подключаем спец.функ
 const bcrypt = require('bcryptjs'); 
 const config = require('config'); 
 const jwt = require('jsonwebtoken'); 
-const { check, validationResult } = require('express-validator'); 
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 const router = Router();
